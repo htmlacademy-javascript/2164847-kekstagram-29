303c65b59a7fe290d2e44a34642ec8fd5493e407
ledygag <mplatonova696@gmail.com>
2023-07-02 22:55:56 +0300

    сделала задание функции возвращаются

--- js/functions.js
+++ js/functions.js
@@ -47,3 +47,45 @@ c = palindrome('Кекс');  // false
 console.log(a);
 console.log(b);
 console.log(c);
+
+function calcMeetingTime(startWork = '', endWork = '', startMeeting = '', meetingDuration = 0) {
+  let [
+    startWorkHours = 0,
+    startWorkMinutes = 0
+  ] = startWork.split(':').map(substr => parseInt(substr, 10));
+
+  startWorkMinutes += startWorkHours * 60;
+
+  let [
+    endWorkHours = 0,
+    endWorkMinutes = 0
+  ] = endWork.split(':').map(substr => parseInt(substr, 10));
+
+  endWorkMinutes += endWorkHours * 60;
+
+  let [
+    startMeetingHours = 0,
+    startMeetingMinutes = 0
+  ] = startMeeting.split(':').map(substr => parseInt(substr, 10));
+
+  startMeetingMinutes += startMeetingHours * 60;
+
+  if(startMeetingMinutes < startWorkMinutes) {
+    return false;
+  }
+
+  if(startMeetingMinutes + meetingDuration > endWorkMinutes) {
+    return false;
+  }
+
+  return true;
+}
+console.log(
+  "module5-task2",
+  calcMeetingTime('08:00', '17:30', '14:00', 90), // true
+  calcMeetingTime('8:0', '10:0', '8:0', 120),     // true
+  calcMeetingTime('08:00', '14:30', '14:00', 90), // false
+  calcMeetingTime('14:00', '17:30', '08:0', 90),  // false
+  calcMeetingTime('8:00', '17:30', '08:00', 900) // false
+);
+
