c2fe1b95c19a52b4dacff2f5f298f04ce7e64d01
ledygag <mplatonova696@gmail.com>
2023-07-16 18:06:29 +0300

    работа с событиями часть2

--- js/drawModal.js
+++ js/drawModal.js
@@ -1,6 +1,17 @@
 const modal = document.querySelector('.big-picture');
 
 export function openModal(photo) {
+  let commentsLimit = 5;
+  const commentsLoader = modal
+    .querySelector('.comments-loader');
+
+  if(commentsLimit < photo.comments.length) {
+    commentsLoader.classList.remove('hidden');
+  } else {
+    commentsLoader.classList.add('hidden');
+    commentsLimit = photo.comments.length;
+  }
+
   modal
     .querySelector('.big-picture__img img')
     .src = photo.url;
@@ -18,36 +29,33 @@ export function openModal(photo) {
     .textContent = photo.likes;
 
   modal
-    .querySelector('.comments-count')
-    .textContent = photo.comments.length;
-
-  modal
     .querySelector('.social__comments')
-    .innerHTML = `
-      ${
-        photo.comments
-          .map(comment => `
-            <li class="social__comment">
-                <img
-                    src="${comment.url}"
-                    alt="${comment.name}"
-                    width="35" height="35"
-                    class="social__picture"
-                  >
-                <p class="social__text">
-                  ${comment.message}
-                </p>
-            </li>
-          `)
-          .join('')
-      }
-    `;
+    .innerHTML = renderComments(photo.comments, commentsLimit);
   modal
     .querySelector('.social__comment-count')
-    .classList.add('hidden');
-  modal
-    .querySelector('.comments-loader')
-    .classList.add('hidden');
+    .innerHTML = renderCommentsCount(commentsLimit, photo.comments.length);
+
+
+  const handler = function() {
+    commentsLimit += 5;
+    if(commentsLimit >= photo.comments.length) {
+      commentsLimit = photo.comments.length;
+      this.classList.add('hidden');
+    }
+    modal
+      .querySelector('.social__comments')
+      .innerHTML = renderComments(photo.comments, commentsLimit);
+    modal
+      .querySelector('.social__comment-count')
+      .innerHTML = renderCommentsCount(commentsLimit, photo.comments.length);
+  }
+
+  commentsLoader.addEventListener('click', handler);
+  document
+    .getElementById('picture-cancel')
+    .addEventListener('click', () => {
+      commentsLoader.removeEventListener('click', handler);
+    })
 
   modal.classList.remove('hidden');
   document.body.classList.add('modal-open');
@@ -56,4 +64,32 @@ export function openModal(photo) {
 export function closeModal() {
   modal.classList.add('hidden');
   document.body.classList.remove('modal-open');
+}
+
+
+function renderComments(comments = [], limit = 0) {
+  return `
+    ${comments
+        .slice(0, limit)
+        .map(comment => `
+          <li class="social__comment">
+              <img
+                  src="${comment.url}"
+                  alt="${comment.name}"
+                  width="35" height="35"
+                  class="social__picture"
+                >
+              <p class="social__text">
+                ${comment.message}
+              </p>
+          </li>
+        `)
+        .join('')
+    }
+`
+}
+
+
+function renderCommentsCount(from, to) {
+  return `${from} из <span class="comments-count">${to}</span> комментариев`
 }
