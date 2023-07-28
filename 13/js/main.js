5d43053645fc53907be0c363a5d509baa4115b16
Keks <keks@htmlacademy.ru>
2023-07-28 22:02:54 +0300

    :heavy_check_mark: Сборка #13

---
+++ 13/js/main.js
@@ -0,0 +1,39 @@
+import './uploadForm.js';
+import { getPosts } from './api.js';
+import { createState } from './state.js';
+import { getRandomItems } from './utils.js';
+import { renderPictures } from './renderPictures.js';
+import { showFilters, bindFiltersChangeHandler, renderFilters, DEFAULT_VALUE, RANDOM_VALUE, DISCUSSED_VALUE } from './filters.js';
+
+const store = createState({
+  posts: [],
+  activeFilter: DEFAULT_VALUE
+}, renderApp);
+
+
+bindFiltersChangeHandler((targetValue) => {
+  store.update({ activeFilter: targetValue });
+});
+
+getPosts()
+  .then((posts) => {
+    store.update({ posts });
+  })
+  .then(showFilters);
+
+function renderApp(state) {
+  switch(state.activeFilter) {
+    case RANDOM_VALUE:
+      renderPictures(getRandomItems(state.posts, 10));
+      break;
+    case DISCUSSED_VALUE:
+      renderPictures(
+        state.posts.sort((a, b) => b.comments.length - a.comments.length)
+      );
+      break;
+    default:
+      renderPictures(state.posts);
+      break;
+  }
+  renderFilters(state.activeFilter);
+}
